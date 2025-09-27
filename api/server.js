2a66027d2098857fc9c8fce0be997f171cbd0469
Megha Kumari <meghasah1642@gmail.com>
2025-09-27 23:12:33 +0530

    Connetion try

--- api/server.js
+++ api/server.js
@@ -146,6 +146,4 @@ app.post('/api/contact', (req, res) => {
   });
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is Running on http://localhost:${PORT}`);
-// });
+module.exports = app;
